d1a93c4d6180ea91d6c6b6bbc454f3753dabf26a
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-19 18:12:56 +0100

    Navbar Completed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { MenuDiamond } from './Icons/MenuDiamond'
 import { Close } from './Icons/Close'
 import { useEffect } from 'react'
@@ -27,6 +27,21 @@ const NavBar = () => {
       const NavVariants = {
         hover: {rotate: 180, transition: {duration: 1.5}}
       }
+
+
+      const container = {
+        hidden: {x: -1000 ,opacity: 0 },
+        show: {
+           x: 0, 
+          opacity: 1,
+          transition: {
+            duration: 2,
+            delayChildren: 0.5,
+            staggerDirection: -1
+          }
+        }
+      }
+      
     
     return (isOpen) ? (
 
@@ -47,11 +62,12 @@ const NavBar = () => {
                 <h1 className='text-2xl font-bold font-cizel'>Masked Optics <span>Photography</span></h1>
                  
                <motion.div 
-               whileHover={{ scale: 1.3 }}
-               whileTap={{ scale: 0.9 }}
                data-isOpen={!isOpen}
                className = ' flex items-center cursor-pointer font-bold font-mons space-x-12' > 
-               {isOpen ? <h4>Menu</h4> : <h4>Close</h4>}
+               {isOpen ? <motion.h4 
+               whileHover={{ scale: 1.3 }}
+               whileTap={{ scale: 1 }}
+               className='text-2xl'>Menu</motion.h4> : <h4>Close</h4>}
                <motion.div
                variants={NavVariants}
                whileHover= 'hover'
@@ -68,7 +84,9 @@ const NavBar = () => {
       
         
       
-      : <> <motion.nav 
+      : (  
+       <>
+        <motion.nav 
       layout
       initial = {{opacity: 0}}
       animate={{ opacity: 1}}
@@ -84,8 +102,9 @@ const NavBar = () => {
                
                className='pt-10 w-[1200px] cursor-pointer font-mons font-bold space-x-12 justify-end items-center z-50 fixed  flex  text-white'>
                  {isOpen ? <h4>Menu</h4> : <motion.h4
-                 whileHover={{ scale: 1.5 }}
-                 whileTap={{ scale: 0.9 }}
+                 className='text-2xl'
+                 whileHover={{ scale: 1.3 }}
+                 whileTap={{ scale: 1 }}
                  >Close</motion.h4>}
                  <motion.div
                variants={NavVariants}
@@ -96,9 +115,10 @@ const NavBar = () => {
             </motion.div>
                     
         </motion.nav>
+    
     <motion.nav  
       layout
-      initial = {{x:-2000, opacity:-2}}
+      initial = {{x:-2000, opacity: 0}}
       animate = {{x:0, opacity: 1}}
       transition={{
         duration: 1,
@@ -107,17 +127,26 @@ const NavBar = () => {
   
       id="menu">
         
-    <div id="menu-items">
-      <div className="menu-item">Home</div>
-      <div className="menu-item">Portfolio</div>
-      <div className="menu-item">About</div>
-      <div className="menu-item">Contact Us</div>
+       
+    <motion.ul
+     variants={container}
+    initial= 'hidden'
+    animate = 'show'
+    id="menu-items">
+      <motion.li className="menu-item">Home</motion.li>
+      <motion.li className="menu-item">Portfolio</motion.li>
+      <motion.li className="menu-item">About</motion.li>
+      <motion.li className="menu-item">Contact Us</motion.li>
    
-    </div>
+    </motion.ul>
     <div id="menu-background-pattern"></div>
     <div id="menu-background-image"></div>
+
   </motion.nav>
+
+
   </>
+  )
 
 
   
