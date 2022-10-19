e3d8d0a93e693dcabf20ab93a732962b34d069db
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-19 22:06:16 +0100

    Exit Animation Added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -20,9 +20,6 @@ const NavBar = () => {
 
     const [isOpen, setIsOpen] = useState(true);
 
-    const resetState = () => {
-        setIsOpen(true);
-      };
 
       const NavVariants = {
         hover: {rotate: 180, transition: {duration: 1.5}}
@@ -32,8 +29,8 @@ const NavBar = () => {
      
       
     
-    return (isOpen) ? (
-
+    return (
+        <>
         <motion.nav 
         initial = {{opacity: 0}}
         animate={{ opacity: 1}}
@@ -44,7 +41,6 @@ const NavBar = () => {
         >
                < motion.div
                layout  
-               onClick={() => setIsOpen(!isOpen)}
                initial = {{y: -200}}
                animate = {{y:0}}
                transition ={{duration: 1}}
@@ -52,12 +48,17 @@ const NavBar = () => {
                 <h1 className=' lg:text-2xl max-w-[200px] lg:max-w-full font-bold font-cizel'>Masked Optics <span>Photography</span></h1>
                  
                <motion.div 
-               data-isOpen={!isOpen}
-               className = ' flex items-center cursor-pointer font-bold font-mons space-x-4'  > 
+               
+               data-isOpen={!isOpen} 
+               onClick={() => setIsOpen(!isOpen)}
+               className = ' flex items-center cursor-pointer font-bold font-mons space-x-4 md:space-x-6'  > 
                {isOpen ? <motion.h4 
                whileHover={{ scale: 1.3 }}
                whileTap={{ scale: 1 }}
-               className='lg:text-2xl'>Menu</motion.h4> : <h4>Close</h4>}
+               className='lg:text-2xl'>Menu</motion.h4> : <motion.h4
+               whileHover={{ scale: 1.3 }}
+               whileTap={{ scale: 1 }}
+               className='lg:text-2xl'>Close</motion.h4>}
                <motion.div
                variants={NavVariants}
                whileHover= 'hover'
@@ -70,41 +71,16 @@ const NavBar = () => {
                     
           
         </motion.nav>
-      )
+   
       
         
       
-      : (  
-       <>
-        <motion.nav 
-        layout
-      initial = {{opacity: 0}}
-      animate={{ opacity: 1}}
-      transition={{
-          duration: 1,
-        }}
-        className = 'flex justify-end max-w-[330px] md:max-w-[1350px]'>
-               < motion.div 
-               layout
-               onClick={resetState} 
-               initial = {{y: -200, opacity: 0}}
-               animate = {{y: 0, opacity: 1}}
-               transition ={{duration: 1}}
-               
-               className='pt-6 lg:pt-10  justify-end cursor-pointer font-mons font-bold space-x-4  items-center z-50 fixed  flex  text-white'>
-                 {isOpen ? <h4>Menu</h4> : <motion.h4
-                 className='lg:text-2xl'
-                 whileHover={{ scale: 1.3 }}
-                 whileTap={{ scale: 1 }}
-                 >Close</motion.h4>}
-                 <motion.div>
-                
-                <MenuDiamond  className = ' max-w-[30px] md:max-w-full animate-pulse diamond' />
-                </motion.div>
-            </motion.div>
-                    
-        </motion.nav>
-    
+       
+        <AnimatePresence>
+        {!isOpen && ( 
+        
+      
+
     <motion.nav  
       initial = {{x:-1000, opacity: 0}}
       animate = {{x:0, opacity: 1}}
@@ -112,13 +88,16 @@ const NavBar = () => {
         duration: 1,
         layout: { duration: 2 }
       }}
+      exit = {{x: -1000, opacity: 0}}
+      
+      
   
       id="menu">
         
        
     <motion.ul
     id="menu-items"
-    className='font-2xl'>
+    className='text-[40px] lg:text-[60px]'>
       <motion.li className="menu-item">Home</motion.li>
       <motion.li className="menu-item">Portfolio</motion.li>
       <motion.li className="menu-item">About</motion.li>
@@ -128,10 +107,14 @@ const NavBar = () => {
     <div id="menu-background-pattern"></div>
     <div id="menu-background-image"></div>
 
-  </motion.nav>
+   </motion.nav>
 
+
+)}
+</AnimatePresence>
 
   </>
+
   )
 
 
