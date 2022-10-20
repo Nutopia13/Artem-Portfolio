1d0359b269d99ccc2cf16ea7e17aeb4cc09e450c
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-20 17:27:24 +0100

    Absolute Position

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
           initial={{ y: -200 }}
           animate={{ y: 0 }}
           transition={{ duration: 1 }}
-          className="pt-6 lg:pt-10  max-w-[370px] md:min-w-[700px] lg:min-w-[1200px] space-x-12 fixed justify-between items-center z-50   flex  text-white"
+          className="pt-6 lg:pt-10  max-w-[370px] md:min-w-[700px] lg:min-w-[1200px] space-x-12 absolute justify-between items-center z-50   flex  text-white"
         >
           <h2 className=" lg:text-2xl max-w-[200px] lg:max-w-full font-bold font-cizel">
             Masked Optics <span>Photography</span>
